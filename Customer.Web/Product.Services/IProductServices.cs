f7730266689d14f631d4ea09c64e49f361791c5f
HARVINDAR SINGH <W9355748@live.tees.ac.uk>
2023-01-15 15:40:58 +0000

    adeded fake data

---
+++ Customer.Web/Product.Services/IProductServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Customer.Web.Product.Services
+{
+    public interface IProductServices
+    {
+        public Task<IEnumerable<ProductDto>> GetProductsAsync(string productname);
+
+        public Task<ProductDto> GetProductAsync(int Id);
+
+    }
+}
---
+++ Customer.Web/Product.Services/ProductServices.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System;
+
+namespace Customer.Web.Product.Services
+{
+    public class ProductServices : IProductServices
+    {
+        private readonly HttpClient _client;
+
+        public ProductServices(HttpClient client)
+
+        {
+            client.BaseAddress = new System.Uri("http://localhost:3733/");
+            client.Timeout = TimeSpan.FromSeconds(5);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            _client = client;
+        }
+
+        public async Task<ProductDto> GetProductAsync(int id)
+        {
+            var response = await _client.GetAsync("api/products/" + id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var product = await response.Content.ReadAsAsync<ProductDto>();
+            return product;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProductsAsync(string productsname)
+        {
+            var uri = "api/products?category=MOV";
+            if (productsname != null)
+            {
+                uri = uri + "&productsname=" + productsname;
+            }
+            var response = await _client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            var products = await response.Content.ReadAsAsync<IEnumerable<ProductDto>>();
+            return products;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
