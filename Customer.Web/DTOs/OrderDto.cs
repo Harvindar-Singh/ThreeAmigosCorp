0e9cbd0286b321c4210e88f873a32741a403a90c
W9355748 <W9355748@TU46985>
2023-01-15 13:29:29 +0000

    Add project files.

---
+++ Customer.Web/DTOs/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Web.DTOs
+{
+    public class OrderDto
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ Customer.Web/DTOs/ReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Web.DTOs
+{
+    public class ReviewDto
+    {
+
+        [Key]
+        public int ReviewId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Description { get; set; }
+    }
+}
